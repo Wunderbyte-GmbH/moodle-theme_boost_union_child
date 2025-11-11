1e12297ef254ec1c08f2754341b8adfbdc28a212
winnidat <thomas.winkler@wunderbyte.at>
2025-11-11 15:20:59 +0100

    Update plugin dependency version to match current

--- version.php
+++ version.php
@@ -27,4 +27,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_boost_union_child';
 $plugin->version = 2025041401;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = ['theme_boost_union' => 2025041403];
+$plugin->dependencies = ['theme_boost_union' => 2025041401];
