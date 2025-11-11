521e067068030d68505dcd796d0d497402b5e547
Thomas Winkler <thomas.winkler@wunderbyte.at>
2025-11-11 15:49:16 +0100

    version bump

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union_child';
-$plugin->version = 2024010100;
+$plugin->version = 2024010101;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = ['theme_boost_union' => 2024100725];
