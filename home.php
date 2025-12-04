f22a0b00031f2a865c738909ed5d0e27270bce6e
Thomas Winkler <thomas.winkler@wunderbyte.at>
2025-12-04 10:09:09 +0100

    test zoom

--- home.php
+++ home.php
@@ -28,9 +28,62 @@ require_login();
 $context = context_system::instance();
 $PAGE->set_context($context);
 $PAGE->set_pagelayout('standard');
-
 $PAGE->set_url('/theme/boost_union_child/home.php');
 
 echo $OUTPUT->header();
 
+global $USER, $DB;
+
+// If you expect a courseid coming from the URL or form, you need this:
+$values = new stdClass();
+$values->courseid = optional_param('courseid', 0, PARAM_INT);
+
+var_dump($values); // --- DUMP #1 ---
+
+if (empty($values->courseid)) {
+    echo '';
+    echo $OUTPUT->footer();
+    exit;
+}
+
+$course = $DB->get_record('course', ['id' => $values->courseid], '*');
+var_dump($course); // --- DUMP #2 ---
+
+if (empty($course)) {
+    echo '';
+    echo $OUTPUT->footer();
+    exit;
+}
+
+$zooms = get_all_instances_in_course('zoom', $course);
+var_dump($zooms); // --- DUMP #3 ---
+
+if (empty($zooms)) {
+    echo '';
+    echo $OUTPUT->footer();
+    exit;
+}
+
+$z = array_shift($zooms);
+var_dump($z); // --- DUMP #4 ---
+
+[$inprogress, $available, $finished] = zoom_get_state($z);
+var_dump($inprogress, $available, $finished); // --- DUMP #5 ---
+
+if (!$available) {
+    echo 'Not available';
+    echo $OUTPUT->footer();
+    exit;
+}
+
+$btntext = get_string('join_meeting', 'mod_zoom');
+$aurl = new moodle_url('/mod/zoom/loadmeeting.php', ['id' => $z->id]);
+var_dump($aurl); // --- DUMP #6 ---
+
+$buttonhtml = html_writer::tag('button', 'Participa', ['type' => 'submit', 'class' => 'btn btn-primary']);
+$buttonhtml .= html_writer::input_hidden_params($aurl);
+$link = html_writer::tag('form', $buttonhtml, ['action' => $aurl->out_omit_querystring(), 'target' => '_blank']);
+echo $link;
+
 echo $OUTPUT->footer();
+
